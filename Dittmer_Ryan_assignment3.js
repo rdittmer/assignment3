34fa5ec1d913180aba8fdcbea344acbc9dcdba54
Ryan Dittmer <rdittmer@fullsail.edu>
2012-01-19 19:24:56 -0500

    finished playerList object, started main playGame function

--- Dittmer_Ryan_assignment3.js
+++ Dittmer_Ryan_assignment3.js
@@ -5,6 +5,14 @@
  
  
  var playerNames;
+ var playerScores;
+ var deathLimit;
+ 
+ playerNames  = [" "," "," "];
+ playerScores = [5,   10 , 110 ];
+ deathLimit   = 5;
+ 
+ 
  var playerList = 
  {
  	 player : [],
@@ -25,11 +33,71 @@
  			nameArray[i] = this.player[i].name;
  		}
  		return nameArray;
+ 	},
+ 	
+ 	getScores: function()
+ 	{
+ 		var scoreArray = [];
+ 		for ( var i = 0; i < this.player.length; ++i )
+ 		{
+ 			scoreArray[i] = this.player[i].score;
+ 		}
+ 		return scoreArray;
+ 	},
+ 	
+ 	setScores: function( scores )
+ 	{
+		for ( var i = 0; i < scores.length; ++i )
+ 		{
+ 			this.player[i].score = scores[i];
+ 		}
+
+ 	},
+ 	
+ 	preGame: function()
+ 	{
+ 		 console.log( "I want to play a First Person Shooter game online. ")
+ 	     console.log( "The game will start and each player gets to play until they reach " + deathLimit + " deaths. \nThe score is then counted and whoever has the highest wins! \nThe players are entering the match:" );
  	}
  	
  }
  
+ 
  playerList.handleData(json);
- playerNames = playerList.getNames();
- console.log(playerNames);
+ playerNames  = playerList.getNames();
+ playerScores = playerList.getScores();
+ playerList.preGame();
+ 
+ 
+ function playGame( playerScores, playerNames, deathLimit )
+ {
+ 	var numKills;
+ 	var totalScore;
+ 	var bestScore;
+ 	var hasBestScore;
+ 	var bestName;
+ 	
+ 	numKills      = 0;
+ 	totalScore    = 0;
+ 	bestScore     = 0;
+ 	hasBestScore  = false;
+ 	
+ 	for ( var i = 0; i < playerScores.length; ++i )
+ 	{
+ 		numKills = getKills( i, deathLimit );
+ 		totalScore   = ( numKills * 100 );
+ 		playerScores[ i ]  = totalScore;
+ 		
+ 		console.log( playerNames[ i ] + "'s total score is " + playerScores[ i ] );
+ 		
+ 		hasBestScore = getBestScore( playerScores[ i ], bestScore, i );
+ 		if ( hasBestScore == true )
+ 		{
+ 			bestScore = playerScores[ i ];
+ 			bestName  = getBestName( playerNames[ i ], winningMsg );
+ 		}
+ 	}	
+ 	console.log( bestName );
+ }
+ 
  
